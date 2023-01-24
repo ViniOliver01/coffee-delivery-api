307594a6fa449e0f5a5f9fd40a76b0d8e715e099
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-01-24 10:40:04 -0300

    create providers

---
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,47 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import { IDateProvider } from "../IDateProvider";
+
+dayjs.extend(utc);
+
+class DayjsDateProvider implements IDateProvider {
+  compareInHours(start_date: Date, end_date: Date): number {
+    const start_date_utc = this.convertToUTC(start_date);
+    const end_date_utc = this.convertToUTC(end_date);
+
+    const diference = dayjs(end_date_utc).diff(start_date_utc, "hours");
+    return diference;
+  }
+
+  compareInDays(start_date: Date, end_date: Date): number {
+    const start_date_utc = this.convertToUTC(start_date);
+    const end_date_utc = this.convertToUTC(end_date);
+
+    const diference = dayjs(end_date_utc).diff(start_date_utc, "days");
+    return diference;
+  }
+
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format();
+  }
+
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
+  }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hours").toDate();
+  }
+
+  verifyIsExpired(expires_date: Date): boolean {
+    const expires_date_utc = this.convertToUTC(expires_date);
+    const isExpired = dayjs(expires_date_utc).isBefore(this.dateNow());
+    return isExpired;
+  }
+}
+
+export { DayjsDateProvider };
