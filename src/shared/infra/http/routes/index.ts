700014ee232133d058c9c877dd1571bb2434006a
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-01-25 13:54:42 -0300

    added index routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
+import { coffeeRoutes } from "./coffee.routes";
 import { passwordRoutes } from "./password.routes";
+import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/users", usersRoutes);
 router.use("/password", passwordRoutes);
+router.use("/coffee", coffeeRoutes);
+router.use("/specifications", specificationsRoutes);
 router.use(authenticateRoutes);
 
 export { router };
