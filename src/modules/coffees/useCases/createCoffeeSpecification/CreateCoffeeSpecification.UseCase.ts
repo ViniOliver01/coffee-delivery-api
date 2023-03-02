34af5396b311de0730c2b106a02fb6b1621e09ff
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-03-02 11:54:21 -0300

    add error message

--- src/modules/coffees/useCases/createCoffeeSpecification/CreateCoffeeSpecification.UseCase.ts
+++ src/modules/coffees/useCases/createCoffeeSpecification/CreateCoffeeSpecification.UseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../shared/errors/AppError";
 import { ICoffeesRepository } from "../../repositories/interfaces/ICoffeesRepository";
 import { ISpecificationsRepository } from "../../repositories/interfaces/ISpecificationsRepository";
 
@@ -19,17 +20,17 @@ class CreateCoffeeSpecificationUseCase {
   async execute({ coffee_id, specifications_ids }: IRequest): Promise<void> {
     const coffee = await this.coffeesRepository.findById(coffee_id);
 
-    // if (!coffee) {
-    //   throw new AppError("Coffee does not exists");
-    // }
+    if (!coffee) {
+      throw new AppError("Café não encontrado");
+    }
 
     const specifications = await this.specificationsRepository.findByIds(
       specifications_ids
     );
 
-    // if (specifications.length === 0) {
-    //   throw new AppError("Specifications does not exists");
-    // }
+    if (specifications.length === 0) {
+      throw new AppError("Especificação não encontrada");
+    }
 
     coffee.specifications = specifications;
 
